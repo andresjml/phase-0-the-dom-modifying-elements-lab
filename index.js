d195e9880908fd3ba21c2819a983a8471fae92dd
Andres <andresjml@gmail.com>
2021-03-20 01:27:57 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code here!
+const main = document.querySelector('main');
+main.remove();
+
+const h1 = document.createElement('h1');
+h1.id ='victory';
+document.body.appendChild(h1);
+
+const newHeader = document.getElementById('victory');
+newHeader.innerHTML = 'Andres is the champion';
+
